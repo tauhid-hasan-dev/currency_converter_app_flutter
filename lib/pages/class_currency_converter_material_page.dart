0724c00dc7450097453edec51ee5f3f04264dfec
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-05-08 15:55:49 +0300

    currency data fetching is working successfully

--- lib/pages/class_currency_converter_material_page.dart
+++ lib/pages/class_currency_converter_material_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart';
+
 // we can not create mutable variable inside a statefulWidget because it has const cosntructor.
 // but we can create mutable variable inside the class which is extended from the State() class.
+// Note: Build function should be less expensive as much as possible
+// Mutable variable should not be used inside the build function
 
+//? Here’s a simplified illustration of where initState() fits into the widget lifecycle:
+/* 
+      1- Widget Creation (constructor is called)
+      2- Initialization (initState() is called)
+      3- Call super.initState() to inherit the parent class’s initialization.
+      4- Perform any custom initialization such as data fetching or setting up listeners.
+      5- Widget Building (build() method is called to describe the widget in terms of other lower-level widgets)
+      6- Widget Rebuilding (if setState() is called, build() method is called again) 
+    
+*/
+
+//? Project Algorithm
 // 1.  Create a variable that stores the converted currency value.
 // 2.  Create a function that multiplies the value given by the textfield
 // 3.  Store the value in the variable that we created
 // 4.  Display the variable.
-
-// Note: Build function should be less expensive as much as possible
-// Mutable variable should not be used inside the build function
 
 class CurrencyConverterPage extends StatefulWidget {
   const CurrencyConverterPage({super.key});
@@ -19,21 +33,43 @@ class CurrencyConverterPage extends StatefulWidget {
 
 class _CurrencyConverterPage extends State<CurrencyConverterPage> {
   double result = 0;
+  double conversionRate = 1;
+
   final TextEditingController textEditingController = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+    fetchCurrencyData();
+  }
+
+  Future<void> fetchCurrencyData() async {
+    var url = Uri.parse('https://www.tcmb.gov.tr/kurlar/today.xml');
+    // print("this is url----> $url");
+    var response = await http.get(url);
+    // print("this is response----> $response");
+    var document = XmlDocument.parse(response.body);
+    // print("this is document----> $document");
+    var usdElement = document
+        .findAllElements('Currency')
+        .firstWhere((element) => element.getAttribute('Kod') == 'USD');
+    // print("this is usdElement----> $usdElement");
+    var usdRate = usdElement.findElements('ForexBuying').first.innerText;
+    // print("this is usdElement----> $usdRate");
+    setState(() {
+      conversionRate = double.parse(usdRate);
+    });
+  }
+
+  void convert() async {
+    await fetchCurrencyData(); // Fetch latest currency rate before conversion
+    double inputAmount = double.tryParse(textEditingController.text) ?? 0;
+    result = inputAmount * conversionRate;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    void convert() {
-      result = double.parse(textEditingController.text) * 80;
-      setState(() {}); // setState triggers a rebuild using the build function
-    }
-
-    @override
-    void dispose() {
-      textEditingController.dispose();
-      super.dispose();
-    }
-
     const border = OutlineInputBorder(
       borderSide: BorderSide(
         color: Color.fromARGB(255, 160, 160, 160),
@@ -60,7 +96,7 @@ class _CurrencyConverterPage extends State<CurrencyConverterPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              result.toString(),
+              'TRY ${result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
               style: const TextStyle(
                   fontSize: 35,
                   fontWeight: FontWeight.bold,
@@ -70,7 +106,9 @@ class _CurrencyConverterPage extends State<CurrencyConverterPage> {
               padding: const EdgeInsets.all(8.0),
               child: TextField(
                 controller: textEditingController,
-                style: const TextStyle(color: Colors.white),
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
                 decoration: const InputDecoration(
                   hintText: "Please enter an amount to convert to USD",
                   hintStyle:
